7a3457c7bfeb252a770bbb81e533726714ca4e79
Su <suce126@126.com>
2019-01-04 00:20:56 +0100

    dd

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -21,7 +21,7 @@ class Register extends React.Component {
         this.setState({ password: event.target.value })
     }
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/register', {
+        fetch('https://dry-coast-73945.herokuapp.com/register', {
             method: 'post',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -17,7 +17,7 @@ class Signin extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://dry-coast-73945.herokuapp.com/signin', {
             method: 'post',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
